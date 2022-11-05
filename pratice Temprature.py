135961c0732245c9315fa72c22fff40badcfc23e
risinggod <Frederik.paulsen@online.de>
2022-11-05 15:56:27 +0100

    Temprature kalkulator

---
+++ pratice Temprature.py
@@ -0,0 +1,5 @@
+tempratur_in_celsius=input("ride down a tempratur in celsius")
+cast=float(tempratur_in_celsius)
+fahrenheit=(cast*9/5)+32
+kelvin=cast+273.15
+print("Fahrenheit:", fahrenheit, "kelvin:", kelvin)
